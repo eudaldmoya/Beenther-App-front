36c5e59b9bf792d0ef55b3947f390abc24e2900c
eudaldmoya <eudald.moya.8@gmail.com>
2023-08-31 12:00:24 +0200

    Initial commit

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../store";
+import App from "./App";
+
+describe("Given an App component", () => {
+  describe("When rendered", () => {
+    test("Then it should show the text 'Hello world!", () => {
+      const expectedText = /beenther!/i;
+
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+      );
+
+      const text = screen.getByText(expectedText);
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/components/App/App.tsx
@@ -0,0 +1,5 @@
+const App = (): React.ReactElement => {
+  return <h1>Beenther!</h1>;
+};
+
+export default App;
