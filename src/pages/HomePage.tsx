20c5ea145f18c29979f2e5c4d6996e4ef1b2b796
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-05 17:50:46 +0200

    Add HomePage structure with login/logout functions

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,31 @@
+import Button from "../components/Button/Button";
+import githubLogo from "../assets/githubLogo.svg";
+import "./HomePage.css";
+import { signInWithPopup } from "firebase/auth";
+import { auth, githubProvider } from "../firebase";
+
 const HomePage = () => {
+  const login = async () => {
+    await signInWithPopup(auth, githubProvider);
+  };
+
   return (
-    <main>
-      <h1>Your destinations</h1>
+    <main className="main-container">
+      <h1 className="login__title">
+        Welcome to <span className="login__title--span">Beenther!</span>
+      </h1>
+      <h2 className="login__description">
+        Save and mark your favorite places in the planet!
+      </h2>
+      <div className="login__button">
+        <Button
+          isActive={true}
+          isCardButton={false}
+          text="Sign In"
+          icon={githubLogo}
+          actionOnClick={login}
+        />
+      </div>
     </main>
   );
 };
