ec13a6d3f10123bb7d41bdb9e195a5cf8f888f31
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-05 17:53:14 +0200

    Add logout function to Header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,14 @@
+import HomePage from "../../pages/HomePage";
 import Header from "../Header/Header";
+import "./App.css";
 
 const App = (): React.ReactElement => {
-  return <Header />;
+  return (
+    <div className="container">
+      <Header />
+      <HomePage />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,20 @@
+import { signOut } from "firebase/auth";
 import beentherLogo from "../../assets/beentherLogo.svg";
 import logoutIcon from "../../assets/logoutIcon.svg";
 import Navigation from "../Navigation/Navigation";
 import "./Header.css";
+import { auth } from "../../firebase";
 
 const Header = (): React.ReactElement => {
+  const logout = async () => {
+    await signOut(auth);
+  };
+
   return (
     <header className="header">
       <div className="title-container">
         <img src={beentherLogo} alt="Beenther logo" />
-        <button>
+        <button onClick={logout}>
           <img src={logoutIcon} alt="logout icon" />
         </button>
       </div>
