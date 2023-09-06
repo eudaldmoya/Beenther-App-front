733a6bfb39630908d4108460e60472fd7928a5bd
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-06 16:21:48 +0200

    Test ProtectedRoute component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,13 @@
 import { signOut } from "firebase/auth";
 import beentherLogo from "../../assets/beentherLogo.svg";
 import logoutIcon from "../../assets/logoutIcon.svg";
+import { auth } from "../../firebase";
 import Navigation from "../Navigation/Navigation";
 import "./Header.css";
-import { auth } from "../../firebase";
-import { useNavigate } from "react-router-dom";
 
 const Header = (): React.ReactElement => {
-  const navigate = useNavigate();
-
   const logout = async () => {
     await signOut(auth);
-
-    navigate("/");
   };
 
   return (
