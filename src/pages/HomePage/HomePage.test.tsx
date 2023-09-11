533264f8606dc5d1bcd7523c8dfb890e88f727df
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-11 10:19:08 +0200

    Fix tests to work with lazy load

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,19 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import HomePage from "./HomePage";
 import { BrowserRouter } from "react-router-dom";
+import { Suspense } from "react";
 
 describe("Given a homepage component", () => {
   describe("When it is rendered", () => {
-    test("Then it should show 'Welcome to Beenther!' inside a heading", () => {
+    test("Then it should show 'Welcome to Beenther!' inside a heading", async () => {
       const title = "Welcome to Beenther!";
 
       render(
         <BrowserRouter>
-          <HomePage />
+          <Suspense>
+            <HomePage />
+          </Suspense>
         </BrowserRouter>,
       );
 
-      const heading = screen.getByRole("heading", { name: title });
+      const heading = await screen.findByRole("heading", { name: title });
 
       expect(heading).toBeInTheDocument();
     });
