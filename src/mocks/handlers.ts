1baf507a4f113a4ec4416913e3462087280c67f8
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-12 14:33:41 +0200

    Test deleteDestinationApi function

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -9,6 +9,17 @@ export const handlers = [
       return res(ctx.status(200), ctx.json({ destinations: destinationsMock }));
     },
   ),
+  rest.delete(
+    `${import.meta.env.VITE_DESTINATIONS_API_URL}${paths.destinations}/${
+      destinationsMock[0]._id
+    }`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({ message: "Destination deleted successfully" }),
+      );
+    },
+  ),
 ];
 
 export const errorHandlers = [
@@ -16,6 +27,14 @@ export const errorHandlers = [
     `${import.meta.env.VITE_DESTINATIONS_API_URL}${paths.destinations}`,
     (_req, res, ctx) => {
       return res(ctx.status(404, "Could not get the destinations"));
+    },
+  ),
+  rest.delete(
+    `${import.meta.env.VITE_DESTINATIONS_API_URL}${paths.destinations}/${
+      destinationsMock[0]._id
+    }`,
+    (_req, res, ctx) => {
+      return res(ctx.status(404, "Could not delete the destination"));
     },
   ),
 ];
