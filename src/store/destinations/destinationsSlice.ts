679cf109dcd73c0043ec09dd04a5bb4856b73455
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-12 10:25:51 +0200

    Add deleteDestinations reducer

--- src/store/destinations/destinationsSlice.ts
+++ src/store/destinations/destinationsSlice.ts
@@ -17,6 +17,15 @@ const destinationsSlice = createSlice({
       ...currentDestinationsState,
       destinations: action.payload,
     }),
+    deleteDestination: (
+      currentDestinationsState: DestinationsState,
+      action: PayloadAction<string>,
+    ): DestinationsState => ({
+      ...currentDestinationsState,
+      destinations: currentDestinationsState.destinations.filter(
+        (destination) => destination._id !== action.payload,
+      ),
+    }),
   },
 });
 
