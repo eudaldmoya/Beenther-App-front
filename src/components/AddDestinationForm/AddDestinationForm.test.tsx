5aaea6868d4594a6e5441dd382460a15176ec790
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-13 19:53:19 +0200

    Test form button enabled/disabled

--- src/components/AddDestinationForm/AddDestinationForm.test.tsx
+++ src/components/AddDestinationForm/AddDestinationForm.test.tsx
@@ -10,6 +10,13 @@ describe("Given an AddDestinationForm component", () => {
   const hImageLabel = "Horizontal image url:";
   const vImageLabel = "Vertical image url:";
 
+  const name = "Angkor Wat";
+  const location = "Siem Reap";
+  const country = "Cambodia";
+  const description = "Nice place";
+  const hImageUrl = "https://www.himg.png";
+  const vImageUrl = "https://www.vimg.png";
+
   describe("When it is rendered", () => {
     test("Then it should show the inputs for name, description, location, country, verticalImageUrl and horizontalImageUrl", () => {
       render(<AddDestinationForm />);
@@ -32,13 +39,6 @@ describe("Given an AddDestinationForm component", () => {
 
   describe("When the user type 'Angkor Wat', 'Siem Reap', 'Cambodia', 'Nice place', 'himg.png', 'vimg.png'", () => {
     test("Then the input should show 'Angkor Wat', 'Siem Reap', 'Cambodia', 'Nice place', 'https://www.himg.png', 'https://www.vimg.png'", async () => {
-      const name = "Angkor Wat";
-      const location = "Siem Reap";
-      const country = "Cambodia";
-      const description = "Nice place";
-      const hImageUrl = "https://www.himg.png";
-      const vImageUrl = "https://www.vimg.png";
-
       render(<AddDestinationForm />);
 
       const nameInput = screen.getByLabelText(nameLabel);
@@ -61,6 +61,44 @@ describe("Given an AddDestinationForm component", () => {
       expect(descriptionInput).toHaveValue(description);
       expect(hImageInput).toHaveValue(hImageUrl);
       expect(vImageInput).toHaveValue(vImageUrl);
+    });
+  });
+
+  describe("When inputs are empty", () => {
+    test("Then it should show a disabled button", () => {
+      const buttonText = "Add Destination";
+
+      render(<AddDestinationForm />);
+
+      const button = screen.getByRole("button", { name: buttonText });
+
+      expect(button).toBeDisabled();
+    });
+  });
+
+  describe("When inputs are filled", () => {
+    test("Then it should show an enabled button", async () => {
+      const buttonText = "Add Destination";
+
+      render(<AddDestinationForm />);
+
+      const nameInput = screen.getByLabelText(nameLabel);
+      const locationInput = screen.getByLabelText(locationLabel);
+      const countryInput = screen.getByLabelText(countryLabel);
+      const descriptionInput = screen.getByLabelText(descriptionLabel);
+      const hImageInput = screen.getByLabelText(hImageLabel);
+      const vImageInput = screen.getByLabelText(vImageLabel);
+
+      await userEvent.type(nameInput, name);
+      await userEvent.type(locationInput, location);
+      await userEvent.selectOptions(countryInput, country);
+      await userEvent.type(descriptionInput, description);
+      await userEvent.type(hImageInput, hImageUrl);
+      await userEvent.type(vImageInput, vImageUrl);
+
+      const button = screen.getByRole("button", { name: buttonText });
+
+      expect(button).toBeEnabled();
     });
   });
 });
