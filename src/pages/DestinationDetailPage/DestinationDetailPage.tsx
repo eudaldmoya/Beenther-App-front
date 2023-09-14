15b21f263c943f422ad0bcffba0d47ffbc2452c1
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-14 22:15:19 +0200

    Create DestinationDetailPage structure

---
+++ src/pages/DestinationDetailPage/DestinationDetailPage.tsx
@@ -0,0 +1,34 @@
+import { lazy, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useDestinationsApi from "../../hook/useDestinationsApi";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { loadSelectedDestinationActionCreator } from "../../store/destinations/destinationsSlice";
+
+export const DestinationDetailPagePreview = lazy(
+  () => import("./DestinationDetailPage"),
+);
+
+const DestinationDetailPage = () => {
+  const { getDestinationByIdApi } = useDestinationsApi();
+  const dispatch = useAppDispatch();
+  const { destinationId } = useParams();
+  const selectedDestination = useAppSelector(
+    (state) => state.destinationsState.selectedDestination,
+  );
+
+  useEffect(() => {
+    (async () => {
+      const selectedDestination = await getDestinationByIdApi(destinationId!);
+
+      dispatch(loadSelectedDestinationActionCreator(selectedDestination));
+    })();
+  }, [destinationId, dispatch, getDestinationByIdApi]);
+
+  return (
+    <>
+      <h1>{selectedDestination?.name}</h1>
+    </>
+  );
+};
+
+export default DestinationDetailPage;
