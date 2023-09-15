2d250a4b1035934882e9d8e82ad48b315e783fa0
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-15 09:57:55 +0200

    Add navigation on card title click

--- src/components/DestinationCard/DestinationCard.tsx
+++ src/components/DestinationCard/DestinationCard.tsx
@@ -4,6 +4,8 @@ import deleteIcon from "../../assets/deleteIcon.svg";
 import { useAppDispatch } from "../../store";
 import { deleteDestinationActionCreator } from "../../store/destinations/destinationsSlice";
 import useDestinationsApi from "../../hook/useDestinationsApi";
+import { NavLink } from "react-router-dom";
+import paths from "../../paths/paths";
 
 interface DestinationCardProps {
   destination: Destination;
@@ -32,10 +34,12 @@ const DestinationCard = ({
       />
       <div className="card__filter"></div>
       <div className="card__info">
-        <div>
-          <h2 className="card__title">{name}</h2>
-          <span className="card__location">{`${location}, ${country}`}</span>
-        </div>
+        <NavLink to={`${paths.destinations}/${_id}`}>
+          <div>
+            <h2 className="card__title">{name}</h2>
+            <span className="card__location">{`${location}, ${country}`}</span>
+          </div>
+        </NavLink>
         <button onClick={handleDeleteClick} aria-label="delete-button">
           <img src={deleteIcon} alt="delete icon" height="42" width="42" />
         </button>
